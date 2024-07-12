67b913ab55b6ad7f40c898c2fe62aa44e8093e4c
Alejandro Díez López <adilovi95@gmail.com>
2024-07-12 10:12:24 +0200

    Hotfix: site y base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    base: 'bracerosdesanmartin',
+    site: 'https://adiezdev.github.io',
+    base: 'bracerosdesanmartin.com',
     integrations: [tailwind()],
 });
